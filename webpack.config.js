6ba1c8681719e69968de06c0683489cb76735a15
araceli <carmona.araceli.4690@gmail.com>
2021-08-18 13:31:23 -0700

    forgot to install webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+//webpack analyzer
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require('path');
 const webpack = require("webpack");
 
@@ -12,6 +14,9 @@ module.exports = {
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", //the report otuputs to an HTML file in the dist folder
+        })
     ],
     mode: 'development'
 };
