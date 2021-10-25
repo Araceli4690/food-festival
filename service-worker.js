55d46afd364d8e05c9038d9c39979c16b1774cdb
araceli <carmona.araceli.4690@gmail.com>
2021-10-24 18:36:34 -0700

    schedule and tickets showing

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,6 @@
-//global constants we need
-const APP_PREFIX = 'Foodfest-';
+const APP_PREFIX = 'FoodEvent-';
 const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
-
-//files we'd like to cache, there is storage limit so prioritize functionality
+const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
     "./index.html",
     "./events.html",
@@ -18,57 +15,53 @@ const FILES_TO_CACHE = [
     "./dist/schedule.bundle.js"
 ];
 
-//self refers to the service-worker object
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {       // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+
+            // You can omit if/else for console.log & put one line below like this too.
+            // return request || fetch(e.request)
+        })
+    )
+})
+
+// Cache resources
 self.addEventListener('install', function (e) {
-    //telling browser to wait until work is complete before terminating service-worker
     e.waitUntil(
-        //fincding specific chaes by na,e
         caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache :' + CACHE_NAME)
+            console.log('installing cache : ' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
         })
     )
-});
+})
 
+// Delete outdated caches
 self.addEventListener('activate', function (e) {
     e.waitUntil(
-        //.keys() returns an array of all cache names & calling those name in keylist
         caches.keys().then(function (keyList) {
-            //capture ones that have app prefix adn save in array cacheKeepList
+            // `keyList` contains all cache names under your username.github.io
+            // filter out ones that has this app prefix to create keeplist
             let cacheKeeplist = keyList.filter(function (key) {
                 return key.indexOf(APP_PREFIX);
-            });
+            })
+            // add current cache name to keeplist
             cacheKeeplist.push(CACHE_NAME);
 
-            return Promise.all(
-                keyList.map(function (key, i) {
-                    if (cacheKeeplist.indexOf(key) === -1) {
-                        console.log('deleting cache:' + keyList[i]);
-                        return caches.delete(keyList[i]);
-                    }
-                })
-            )
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            }));
         })
-    )
-})
-
-//telling app how to retrieve info from the cache
-//listen for fetch event
-self.addEventListener('fetch', function (e) {
-    //log url of requested resource
-    console.log('fetch request:' + e.request.url)
-    //how we will respond to the request
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) {
-                //if cache is available respond with
-                console.log('responding with cache:' + e.request.url)
-                return request
-            } else {
-                //if there are no cache, try fetching request
-                console.log('file is not caches, fetching:' + e.request.url)
-                return fetch(e.request)
-            }
-        })
-    )
-})
+    );
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
-//webpack analyzer
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-//webpack manifest
-const WebpackPwaManifest = require("webpack-pwa-manifest");
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const webpack = require("webpack");
 
-module.exports = {
+const config = {
     entry: {
         app: './assets/js/script.js',
         events: './assets/js/events.js',
@@ -13,63 +12,60 @@ module.exports = {
         tickets: './assets/js/tickets.js'
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'
+        filename: '[name].bundle.js',
+        path: `${__dirname}/dist`
     },
     module: {
         rules: [
             {
-                //identifying files to pre-process using regex, in this case any images
-                test: /\.jpg$/i,
-                use: [{
-                    //webpack file-loader being implemented
-                    loader: 'file-loader',
-                    options: {
-                        esModule: false,
-                        //return name of file with file extension
-                        name(file) {
-                            return "[path][name].[ext]"
-                        },
-                        //changes assigment url
-                        publicPath: function (url) {
-                            return url.replace("../", "/assets")
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name(file) {
+                                return '[path][name].[ext]';
+                            },
+                            publicPath(url) {
+                                return url.replace('../', '/assets/');
+                            }
                         }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
                     }
-                },
-                {
-                    //image loader reduces image size
-                    loader: 'image-webpack-loader'
-                }
                 ]
             }
         ]
     },
     plugins: [
         new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
+            $: 'jquery',
+            jQuery: 'jquery'
         }),
         new BundleAnalyzerPlugin({
-            analyzerMode: "static", //the report otuputs to an HTML file in the dist folder
+            analyzerMode: 'static'
         }),
-        //new used when invoking a constructor function
         new WebpackPwaManifest({
-            name: "Food Event",
-            short_name: "Foodies",
-            description: "An app that allows you to view upcoming food events.",
-            //specifying homepage for the PWA
-            start_url: "../index.html",
-            background_color: "#01579b",
-            theme_color: "#ffffff",
+            name: 'Food Event',
+            short_name: 'Foodies',
+            description: 'An app that allows you to view upcoming food events.',
+            start_url: '../index.html',
+            background_color: '#01579b',
+            theme_color: '#ffffff',
             fingerprints: false,
             inject: false,
-            //app icon 
-            icons: [{
-                src: path.resolve("assets/img/icons/icon-512x512.png"),
-                sizes: [96, 128, 192, 256, 384, 512],
-                destination: path.join("assets", "icons")
-            }]
+            icons: [
+                {
+                    src: path.resolve('assets/img/icons/icon-512x512.png'),
+                    sizes: [96, 128, 192, 256, 384, 512],
+                    destination: path.join('assets', 'icons')
+                }
+            ]
         })
     ],
     mode: 'development'
 };
+
+module.exports = config;
